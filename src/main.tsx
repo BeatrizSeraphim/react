20a527e798bc62c3814e9530b5ab4043d793cb8f
Beatriz Seraphim <beatrizoseraphim@gmail.com>
2023-05-15 17:03:34 -0300

    Blog Pessoal

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import Home from './paginas/home/Home';
+
+ReactDOM.render(
+<React.StrictMode>
+<App />
+</React.StrictMode>,
+document.getElementById('root') as HTMLElement
+);
